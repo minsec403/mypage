4d53e74093c81021e4906e26312a6218b9036195
hanminwoo <44539141+hmw000403@users.noreply.github.com>
2022-01-27 17:06:27 +0900

    새로만듬

--- User/src/main/java/com/user/UserApplication.java
+++ User/src/main/java/com/user/UserApplication.java
@@ -1,13 +1,13 @@
-package com.mypage;
+package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PageApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PageApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 
 }
